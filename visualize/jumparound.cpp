38778828ff49f49403ef4d0766ada28cfe39f404
Ryan C. Gordon <icculus@icculus.org>
2005-02-13 23:13:57 +0000

    Added JumpAround profile testing program.

---
+++ visualize/jumparound.cpp
@@ -0,0 +1,85 @@
+/*
+ * Hacky program to profile fragmap snapshot request speed.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <time.h>
+
+#include "dumpfile.h"
+
+class ProgressNotifyNoOp : public ProgressNotify
+{
+public:
+    ProgressNotifyNoOp() {}
+    virtual void update(const char *str, int percent)
+    {
+    } // update
+};
+
+
+// !!! FIXME: cut and paste from the client code.
+static struct timeval tickbase;
+static inline void reset_tick_base(void)
+{
+    gettimeofday(&tickbase, NULL);
+} /* reset_tick_base */
+
+static inline tick_t get_ticks(void)
+{
+    struct timeval curtime;
+    gettimeofday(&curtime, NULL);
+    return( (tick_t) ( ((curtime.tv_sec - tickbase.tv_sec) * 1000) +
+                       ((curtime.tv_usec - tickbase.tv_usec) / 1000) ) );
+} /* get_ticks */
+
+
+static void jump_around(const char *filename, DumpFile &df)
+{
+    const uint32 ITERATIONS = 3;
+    uint32 opcount = df.getOperationCount();
+    tick_t ticks = 0;
+
+    printf("%s: %u operations total.\n", filename, (unsigned int) opcount);
+
+    printf(" + linear fragmap seek...\n");
+    ticks = 0;
+    for (uint32 iter = 0; iter < ITERATIONS; iter++)
+    {
+        reset_tick_base();
+        for (uint32 i = 0; i < opcount; i++)
+        {
+            size_t nc = 0;
+            df.fragmapManager.get_fragmap(&df, i, nc);
+        } // for
+        ticks += get_ticks();
+    } // for
+
+    printf(" +  (%d ticks, %d iterations == %d ticks per iteration\n",
+            (int) ticks, (int) ITERATIONS, (int) (ticks / ITERATIONS));
+} // jump_around
+
+
+int main(int argc, char **argv)
+{
+    for (int i = 1; i < argc; i++)
+    {
+        try
+        {
+            ProgressNotifyNoOp pn;
+            DumpFile df(argv[i], pn);
+            jump_around(argv[i], df);
+        } // try
+
+        catch (const char *err)
+        {
+            printf("Error processing %s: %s\n", argv[i], err);
+        } // catch
+    } // for
+
+    return(0);
+} // main
+
+// end of jumparound.cpp ...
+
